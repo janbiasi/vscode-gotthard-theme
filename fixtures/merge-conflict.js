366bf8ee46470eddbfea274c3a8ba3ca419fedaa
Jan R. Biasi <biasijan@gmail.com>
2021-08-06 23:40:37 +0200

    refactor: improve css theme

--- fixtures/merge-conflict.js
+++ fixtures/merge-conflict.js
@@ -3,3 +3,12 @@ var thisIsMyBranch = 20
 =======
 var thisIsAnotherBranch = 20;
 >>>>>>> someOtherBranch
+
+(() => {
+    console.log("Hello world!");
+})();
+
+document.getElementById("app").addEventListener("click", function (ev) {
+    ev.preventDefault();
+    alert("App clicked!");
+});
--- fixtures/test/asdf.js
+++
@@ -1,7 +0,0 @@
-var x = 10;
-
-function getSomeTea(input) {
-  return input * 30;
-}
-
-exports.getSomeTea = getSomeTea;
