fc0637b7f474a017991b60a1f3b124aef5cef714
Jan Biasi <jan.biasi@sgkbo365.onmicrosoft.com>
2021-08-10 17:20:40 +0200

    ci: adjust release flow

--- release.config.js
+++ release.config.js
@@ -3,12 +3,12 @@ module.exports = {
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
-        [
-            '@semantic-release/github',
-            {
-                assets: ['./bin/gotthard.vsix'],
-            },
-        ]
+        '@semantic-release/changelog',
+        "@semantic-release/npm",
+        ["@semantic-release/git", {
+            "assets": ["./bin/gotthard.vsix", "docs", "package.json", "CHANGELOG.md"],
+            "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+        }]
     ],
     preset: 'angular',
 }
