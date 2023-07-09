3fa1bc59d1bf67ee75a1b5207f45dcac183a3a95
Jan Biasi <janbiasi@users.noreply.github.com>
2023-07-09 18:51:35 +0200

    fix: update node version to v20, bump dependencies

--- src/generator.ts
+++ src/generator.ts
@@ -1,27 +1,28 @@
-import { resolve } from 'path';
-import { readFile, writeFile, mkdirp } from 'fs-extra';
-import { render } from 'eta';
+import { resolve } from 'node:path';
+import { writeFile, mkdirp } from 'fs-extra';
+import { Eta } from 'eta';
 import { themes, transformThemeToTemplateVariables } from './themes';
 
 const SOURCE_ROOT = resolve(__dirname);
+const TEMPLATE_PATH = resolve(SOURCE_ROOT, 'templates');
 const TARGET_DIR = resolve(__dirname, '../dist/');
-const BASE_TEMPLATE_PATH = resolve(SOURCE_ROOT, 'templates/theme.template.json');
+
+const templateEngine = new Eta({
+            views: TEMPLATE_PATH
+        });
 
 (async () => {
     await mkdirp(TARGET_DIR);
-
-    console.log('Loading base template from %s', BASE_TEMPLATE_PATH);
-    const templateContent = await readFile(BASE_TEMPLATE_PATH, 'utf8');
 
     for (const theme of themes) {
         const payload = transformThemeToTemplateVariables(theme);
 
         console.log(`Generating theme "${theme.name}" ...`);
-        const result = render(templateContent, payload);
+        const result = templateEngine.render('./theme.template.json', payload);
 
         console.log(`Writing compiled theme "${theme.name}" ...`);
         await writeFile(resolve(TARGET_DIR, `${theme.id}.json`), result);
 
-        console.log(`${theme.name} ready!`);
+        console.log(`${theme.name} generated successfully!`);
     }
 })();
